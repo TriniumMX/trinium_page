69a7490b469b07fa58f51cfff975b5ae3b657d29
Isra Basurto <72462952+israbasurto@users.noreply.github.com>
2026-04-16 14:51:13 -0600

    Actualizacion de ubicacion

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -39,7 +39,7 @@ const Footer = () => {
               </a>
               <div className="flex items-center gap-3">
                 <MapPin className="w-5 h-5" />
-                <span>México</span>
+                <span>San Juan del Río, Querétaro, México</span>
               </div>
             </div>
           </div>
